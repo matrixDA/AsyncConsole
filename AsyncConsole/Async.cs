fa9872f18ba262c34a57d0e3d9a1ededcca1debb
adhik <adhik@DINESH>
2023-10-12 19:53:20 -0400

    Add project files.

---
+++ AsyncConsole/Async.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncConsole
+{
+    public class Async
+    {
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+        public async Task<int> sumAsync(int a, int b)
+        {
+            return await Task.Run(() => a + b);
+        }
+
+        public async Task ReadBook(string file)
+        {
+            var lines = await Task.FromResult(File.ReadAllLinesAsync(file));
+            var linesWithoutPunctuation = lines.Result.Where(x => x != string.Empty)
+                
+                .AsParallel()
+                    .Select(x => x.ToLower().Trim().Replace(",", "")
+                    .Replace(":", "").Replace(".", "").Replace("*", ""));
+
+            var arrayOfWords = linesWithoutPunctuation.SelectMany(x => x.Split(" "));
+
+            Dictionary<string, int> wordDic = new Dictionary<string, int>();
+
+            arrayOfWords.ToList().ForEach(x =>
+            {
+                if (wordDic.ContainsKey(x))
+                {
+                    wordDic[x]++;
+                }
+                else
+                {
+                    wordDic.Add(x, 1);
+                }
+            });
+
+            var top10 = wordDic.Where(x => !string.IsNullOrWhiteSpace(x.Key))
+                .OrderByDescending(x => x.Value).Take(10);
+
+            foreach (var word in top10) 
+            { 
+                Console.WriteLine(word.Key + " - " + word.Value);
+            }
+        }
+    }
+}
---
+++ AsyncConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsyncConsole;
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Hello, World!");
+
+Async sync = new Async();
+var sum = sync.sum(5, 7);
+Console.WriteLine(sum);
+
+Console.WriteLine("************************************");
+
+var sumAsync = sync.sumAsync(5, 7);
+Console.WriteLine(sumAsync.Result);
+
+
+// create list of tasks
+List<Task> tasks = new List<Task>();
+tasks.Add(sumAsync);
+
+var filename = "C:\\Users\\adhik\\Downloads\\AsyncConsole\\AsyncConsole\\myadventure.txt";
+var worktask = sync.ReadBook(filename);
+tasks.Add(worktask);
+
+Task ts = await Task.FromResult(Task.WhenAll(tasks));
+if (ts.Status == TaskStatus.RanToCompletion)
+{
+    Console.WriteLine("All process sucessfylly completed");
+}
